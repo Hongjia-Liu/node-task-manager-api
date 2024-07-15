69d816b13cc9c791e86dd5043cc9e2ff496d374c
Hongjia-Liu <hongjia_liu@outlook.com>
2024-07-16 00:08:21 +0800

    feat: server init

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Task Manager API");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Node Task Manager API");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port 3000`);
+});
