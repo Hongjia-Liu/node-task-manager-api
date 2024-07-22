bc0eb4f58d253d5314faf95cadd5379d844bcec1
Hongjia-Liu <hongjia_liu@outlook.com>
2024-07-23 07:05:08 +0800

    feat: implement deleteTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -36,8 +36,18 @@ const updateTask = (req, res) => {
   res.send("updateTask");
 };
 
-const deleteTask = (req, res) => {
-  res.send("deleteTask");
+const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `No task with id : ${req.params.id}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
